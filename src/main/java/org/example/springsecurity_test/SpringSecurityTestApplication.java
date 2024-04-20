5efa41b108a194aaedf2f1e45f0624b3cd94de4f
Gary <ch66971542@gmail.com>
2024-04-20 23:27:36 +0800

    init

---
+++ src/main/java/org/example/springsecurity_test/SpringSecurityTestApplication.java
@@ -0,0 +1,13 @@
+package org.example.springsecurity_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/springsecurity_test/SpringSecurityTestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springsecurity_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
