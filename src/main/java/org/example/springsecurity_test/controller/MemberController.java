abe5983637e90047fb7dd20d6f65c48df61509ba
Gary <ch66971542@gmail.com>
2024-04-21 23:51:17 +0800

    實作 登入API

--- src/main/java/org/example/springsecurity_test/controller/MemberController.java
+++ src/main/java/org/example/springsecurity_test/controller/MemberController.java
@@ -1,12 +1,17 @@
 package org.example.springsecurity_test.controller;
 
+
 import org.example.springsecurity_test.dao.MemberDao;
 import org.example.springsecurity_test.model.Member;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
 
 @RestController
 public class MemberController {
@@ -22,5 +27,13 @@ public class MemberController {
 
         Integer memberId = memberDao.createMember(member);
         return "註冊成功";
+    }
+
+    @PostMapping("/userLogin")
+    public String userLogin(Authentication authentication){
+        // 取得使用者帳號
+        String name = authentication.getName();
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+        return "登入成功 ! 帳號 " + name + " 的權限為 " + authorities;
     }
 }
--- src/main/java/org/example/springsecurity_test/security/MySecurityConfig.java
+++ src/main/java/org/example/springsecurity_test/security/MySecurityConfig.java
@@ -5,8 +5,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -23,13 +23,22 @@ public class MySecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         return http
+                // 設定 Session 的創建機制
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.ALWAYS)
+                )
                 .csrf(csrf -> csrf.disable())
 
                 .httpBasic(Customizer.withDefaults())
                 .formLogin(Customizer.withDefaults())
 
                 .authorizeHttpRequests(request -> request
+                        // 註冊功能
                         .requestMatchers("/register").permitAll()
+
+                        // 登入功能
+                        .requestMatchers("/userLogin").authenticated()
+
                         .anyRequest().authenticated()
                 )
 
