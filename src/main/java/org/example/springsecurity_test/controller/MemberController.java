f3fbf23dd79daca6a03eec64e6cf32b6d0f5d817
Gary <ch66971542@gmail.com>
2024-04-21 23:28:52 +0800

    利用 BCrypt 實作密碼加密

--- src/main/java/org/example/springsecurity_test/controller/MemberController.java
+++ src/main/java/org/example/springsecurity_test/controller/MemberController.java
@@ -3,6 +3,7 @@ package org.example.springsecurity_test.controller;
 import org.example.springsecurity_test.dao.MemberDao;
 import org.example.springsecurity_test.model.Member;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,8 +13,12 @@ public class MemberController {
     @Autowired
     private MemberDao memberDao;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
     @PostMapping("/register")
     public String register(@RequestBody Member member){
+        String hashedPassword = passwordEncoder.encode(member.getPassword());
+        member.setPassword(hashedPassword);
 
         Integer memberId = memberDao.createMember(member);
         return "註冊成功";
