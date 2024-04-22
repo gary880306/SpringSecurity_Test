5f5b8e0fcc93d61892246c833a31b2a249616fdf
Gary <ch66971542@gmail.com>
2024-04-22 21:33:19 +0800

    實作 Filter - 紀錄使用者嘗試登入時間

---
+++ src/main/java/org/example/springsecurity_test/security/MyFilter.java
@@ -0,0 +1,27 @@
+package org.example.springsecurity_test.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.Date;
+
+
+public class MyFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String uri = request.getRequestURI();
+        if(uri.equals("/userLogin")){
+            String userAgent = request.getHeader("User-Agent");
+            Date now = new Date();
+
+            System.out.println("使用者於 " + now + " 嘗試從 " + userAgent + " 登入 ");
+        }
+
+        filterChain.doFilter(request,response);
+    }
+}
--- src/main/java/org/example/springsecurity_test/security/MySecurityConfig.java
+++ src/main/java/org/example/springsecurity_test/security/MySecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -28,6 +29,8 @@ public class MySecurityConfig {
                         .sessionCreationPolicy(SessionCreationPolicy.ALWAYS)
                 )
                 .csrf(csrf -> csrf.disable())
+
+                .addFilterBefore(new MyFilter(), BasicAuthenticationFilter.class)
 
                 .httpBasic(Customizer.withDefaults())
                 .formLogin(Customizer.withDefaults())
